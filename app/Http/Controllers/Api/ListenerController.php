282f8341fa9ecccaf8d47e7afbc83c6023229f48
fadilatur11 <fadhilatur7@gmail.com>
2021-04-06 16:08:45 +0700

    header

--- app/Http/Controllers/Api/ListenerController.php
+++ app/Http/Controllers/Api/ListenerController.php
@@ -12,26 +12,26 @@ class ListenerController extends Controller
     function orderNotification(Request $request)
     {
         Storage::disk('local')->put('order-notification-content.txt', $request->getContent());
-        Storage::disk('local')->put('order-notification-header.txt', json_encode($request->header()));
+        Storage::disk('local')->put('order-notification-header.txt', $request->header("Authorization-Hmac"));
     }
 
 
     function orderStatus(Request $request)
     {
         Storage::disk('local')->put('order-status-content.txt', $request->getContent());
-        Storage::disk('local')->put('order-status-header.txt', json_encode($request->header()));
+        Storage::disk('local')->put('order-status-header.txt', $request->header("Authorization-Hmac"));
     }
 
     function orderCancellation(Request $request)
     {
         Storage::disk('local')->put('order-cancellation-content.txt', $request->getContent());
-        Storage::disk('local')->put('order-cancellation-header.txt', json_encode($request->header()));
+        Storage::disk('local')->put('order-cancellation-header.txt', $request->header("Authorization-Hmac"));
     }
 
     function orderConfirmDeliveryNotification(Request $request)
     {
         Storage::disk('local')->put('order-confirmDelivery-content.txt', $request->getContent());
-        Storage::disk('local')->put('order-confirmDelivery-header.txt', json_encode($request->header()));
+        Storage::disk('local')->put('order-confirmDelivery-header.txt', $request->header("Authorization-Hmac"));
     }
 
     function orderRequestCancellation(Request $request)
