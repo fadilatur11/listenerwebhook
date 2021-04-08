bab3487b91f447b75c13370f32eafc7fe64fe861
fadilatur11 <fadhilatur7@gmail.com>
2021-04-08 08:45:01 +0700

    update

--- app/Http/Middleware/AuthorizationHmac.php
+++ app/Http/Middleware/AuthorizationHmac.php
@@ -22,14 +22,14 @@ class AuthorizationHmac
         $calculate_hmac = hash_hmac('sha256', $payload, $secret);
 
         if ($request->header('Authorization') != $calculate_hmac) {
+            Storage::disk('local')->put('order-notification-content.txt', 'notfound');
+            Storage::disk('local')->put('order-notification-header.txt', 'notfound');
             return response()->json([
                 'error' => [
                     'message' => 'Your Web Secret invalid',
                     'status_code' => 406
                     ]
                 ],406);
-            Storage::disk('local')->put('order-notification-content.txt', 'notfound');
-            Storage::disk('local')->put('order-notification-header.txt', 'notfound');
         }
 
         return $next($request);
