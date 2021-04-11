02ca79d041a4ce4dc51711b1c963d1864461c9ab
fadilatur11 <fadhilatur7@gmail.com>
2021-04-11 16:44:30 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['hmacVerify'])->group(function() {
+// Route::middleware(['hmacVerify'])->group(function() {
     Route::post('order-notification',[ListenerController::class, 'orderNotification']);
     Route::post('order-status',[ListenerController::class, 'orderStatus']);
     Route::post('order-cancellation',[ListenerController::class, 'orderCancellation']);
@@ -27,4 +27,4 @@ Route::middleware(['hmacVerify'])->group(function() {
     Route::post('order-request-cancel',[ListenerController::class, 'orderRequestCancellation']);
     Route::post('product/edit',[ListenerController::class,'productEdit']);
     Route::post('product/change',[ListenerController::class,'productChange']);
-});
+// });
